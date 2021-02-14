beab5c5d9d4290a154972cbd3d9624c82fb24f72
Casimiro <cp.ciancimino@gmail.com>
2021-02-15 00:07:18 +0100

    edit: uncomment section authorPage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {searchBy, getDataFrom} from './searchBy.js';
 import createCard from './card.js';
 
-
+const limitRange = (text) => {
+  return (text.length <= 32) ? text : text.slice(0, 32)+'...';
+}
 
 // Init
 const states = {
@@ -15,11 +17,11 @@ const states = {
 const albrechtDurerIDs = searchBy('Albrecht Dürer');
 
 albrechtDurerIDs.then(IDs => {
-  IDs.splice(0,15).forEach(ID => {
+  IDs.splice(0,25).forEach(ID => {
     const artworks = getDataFrom(ID);
     
     artworks.then((artwork) => {
-      createCard(artwork.primaryImageSmall, artwork.title);
+      createCard(artwork.primaryImageSmall, limitRange(artwork.title));
     })
   })
 })
